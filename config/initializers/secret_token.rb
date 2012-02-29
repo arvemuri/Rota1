a754fc530e2d6a54f457f2e1a32e455fedc7ceed
Tasneem Jahan <tjahan@cmu.edu>
2012-02-29 11:55:20 +0300

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rota::Application.config.secret_token = '45b4c50ffc23da30b42e468a9b4c61090db63171bb89d08aee8c3f5b331d72c9aa32138b8049c3ad252ba8d50a593d4ee5925c504e9232297ed6690dec5b6530'
